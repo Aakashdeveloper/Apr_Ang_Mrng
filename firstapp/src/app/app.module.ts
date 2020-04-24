20bc6fa14109dd1b9ccd5d7bead4bdfd4bec272e
aakashDeveloper <ahanda205@gmail.com>
2020-04-24 01:31:05 +0100

    a

--- firstapp/src/app/app.module.ts
+++ firstapp/src/app/app.module.ts
@@ -1,16 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import { HttpClientModule} from '@angular/common/http';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 
 @NgModule({
+  // All your Module & Routing Will be declare here
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+
+  // All Component, Pipe & Directive  Will be declare here
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
+
+  // One and One first component
+  bootstrap: [
+    AppComponent
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+
+  // All Service
+  providers: []
 })
-export class AppModule { }
+
+
+export class AppModule {
+
+}
